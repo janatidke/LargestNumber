9c9ccf63eae8635178aa56035ce4a954f6b309e6
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 01:49:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Enter value of a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of c:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("Largest Number is:"+a );
+
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("Largest Number is:"+b);
+            }
+            else {
+                Console.WriteLine("Largest Number is:"+c);
+            }
+            
+        }
+    }
+}
